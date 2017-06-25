52ff4a9288e38d472d4d1b3754feb386e8afed7d
Fred Morrison <fpmorrison@verizon.net>
2017-06-25 09:00:54 -0400

    fixed inconsistent quote character usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Report Viewer";
+  title = 'Report Viewer';
   viewerContainerStyle = {
     position: 'absolute',
     top: '37px',
