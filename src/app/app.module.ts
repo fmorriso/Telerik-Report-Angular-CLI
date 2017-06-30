b878755cecae0c8f6dc34f07a665bb30e5d0166f
Fred Morrison <fpmorrison@verizon.net>
2017-06-30 13:57:16 -0400

    preparing to deploy to Azure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { TelerikReportingModule } from '@progress/telerik-angular-report-viewer';
+import {AppComponent} from './app.component';
+import {TelerikReportingModule} from '@progress/telerik-angular-report-viewer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    TelerikReportingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        TelerikReportingModule
+    ],
+    providers: [
+        {provide: LocationStrategy, useClass: PathLocationStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
